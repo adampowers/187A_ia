d5b5e1a6d20dc8e0ffc24e61c7050f63131f5a55
Adam Powers <a2powers@ucsd.edu>
2012-02-26 23:10:16 -0800

    Quasi-final push.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ $('#backgrounds').nivoSlider({
+ 	effect: 'fade',
+ 	animSpeed: 500,
+ 	pauseTime: 10000,
+ 	directionNav: false,
+ 	controlNav: false
+
+
+ });
+  
+});
